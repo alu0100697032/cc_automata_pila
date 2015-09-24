b9f7d36d7adde63a01b6cac81fbd371254a2c692
Víctor Hernández Pérez <alu0100697032@ull.edu.es>
2015-09-24 11:18:23 +0100

    First commit

---
+++ src/Automata.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class Automata {
+
+	private ArrayList<String> conjuntoQ;
+	private ArrayList<String> conjuntoSingma;
+	private ArrayList<String> conjuntoR;
+	private String estadoInicial;
+	private String simboloInicialPila;
+	private ArrayList<String> conjuntoF;
+	private ArrayList<ArrayList<String>> conjuntoTransiciones;
+	
+	public Automata(){
+		System.out.println("Introduzca el nombre del fichero con los datos del autómata:");
+		
+	}
+}
