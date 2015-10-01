5dd29fdcabb4e9986a57c0a040e0437aa069247f
Victor Hernandez <alu0100697032@ull.edu.es>
2015-10-01 11:20:27 +0100

    Almost finished, change recognition of final states

--- src/Automata.java
+++ src/Automata.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-
+import java.util.Stack;
 
 public class Automata {
 
@@ -20,73 +20,79 @@ public class Automata {
 	private String simboloInicialPila;
 	private ArrayList<String> conjuntoF;
 	private ArrayList<String[]> conjuntoTransiciones;
-	
+
+	private String estadoActual;
+	private Stack<String> pila;
+
 	/*
 	 * CONSTRUCTOR
 	 */
-	 
-	public Automata() throws IOException{
-		
+
+	public Automata() throws IOException {
+
 		conjuntoQ = new ArrayList<String>();
 		conjuntoSingma = new ArrayList<String>();
 		conjuntoR = new ArrayList<String>();
 		conjuntoF = new ArrayList<String>();
 		conjuntoTransiciones = new ArrayList<String[]>();
+		pila = new Stack<String>();
 		
 		String nombreFichero;
 		File ruta;
-		Scanner imputUsuario = new Scanner(System.in);
-		//Pedir el fichero al usuario
-		//System.out.println("Introduzca el nombre del fichero con los datos del autómata:");
-		//nombreFichero = imputUsuario.nextLine(); (para versión final)
-		imputUsuario.close();
-		nombreFichero = "automata1.txt"; //para versión de desarrollo
+		// Scanner imputNombreFichero = new Scanner(System.in);
+		// Pedir el fichero al usuario
+		// System.out.println("Introduzca el nombre del fichero con los datos del autómata:");
+		// nombreFichero = imputNombreFichero.nextLine(); (para versión final)
+		// imputNombreFichero.close();
+		nombreFichero = "automata2.txt"; // para versión de desarrollo
 		ruta = new File(nombreFichero);
-		//Almacenar la informacion del fichero
+		// Almacenar la informacion del fichero
 		String textoFichero;
 		FileReader leerFichero = new FileReader("./src/" + ruta);
 		BufferedReader bufferLectura = new BufferedReader(leerFichero);
 		int linea = 0;
-		while((textoFichero = bufferLectura.readLine())!=null) {
-			if(textoFichero.matches("#.*"))
+		while ((textoFichero = bufferLectura.readLine()) != null) {
+			if (textoFichero.matches("#.*"))
 				continue;
-			else if(textoFichero.matches("\b*"))
+			else if (textoFichero.matches("\b*"))
 				continue;
-			else{
-				if(linea >= 6){
+			else {
+				if (linea >= 6) {
 					String separarEspacios[] = textoFichero.split(" ");
 					conjuntoTransiciones.add(separarEspacios);
-				}else{
+				} else {
 					String separarEspacios[] = textoFichero.split(" ");
-					for(int i = 0; i < separarEspacios.length; i++){
-						if(separarEspacios[i].matches("#.*"))
+					for (int i = 0; i < separarEspacios.length; i++) {
+						if (separarEspacios[i].matches("#.*"))
 							break;
-						else{
-							if(linea == 0)
+						else {
+							if (linea == 0)
 								conjuntoQ.add(separarEspacios[i]);
-							else if(linea == 1)
+							else if (linea == 1)
 								conjuntoSingma.add(separarEspacios[i]);
-							else if(linea == 2)
+							else if (linea == 2)
 								conjuntoR.add(separarEspacios[i]);
-							else if(linea == 3)
+							else if (linea == 3)
 								estadoInicial = separarEspacios[i];
-							else if(linea == 4)
+							else if (linea == 4)
 								simboloInicialPila = separarEspacios[i];
-							else if(linea == 5)
+							else if (linea == 5)
 								conjuntoF.add(separarEspacios[i]);
 						}
 					}
-				}
+				}// END IF ELSE INTERIOR
 				linea++;
-			}
-		}
+			}// END IF ELSE EXTERIOR
+		}// END WHILE
 		bufferLectura.close();
-		
+		pila.push(simboloInicialPila);
+		estadoActual = estadoInicial;
 	}
+
 	/*
 	 * MOSTRAR INFORMACION DEL AUTOMATA
 	 */
-	public void mostrarInformacionAutomata(){
+	public void mostrarInformacionAutomata() {
 		System.out.println("Información del autómata con pila");
 		System.out.println();
 		System.out.println("Estado inicial: " + estadoInicial);
@@ -96,21 +102,67 @@ public class Automata {
 		System.out.println("Conjunto R: " + conjuntoR.toString());
 		System.out.println("Conjunto F: " + conjuntoF.toString());
 		System.out.println("Conjunto de transiciones:");
-		for(int i = 0; i < conjuntoTransiciones.size(); i++)
+		for (int i = 0; i < conjuntoTransiciones.size(); i++)
 			System.out.println(Arrays.toString(conjuntoTransiciones.get(i)));
 	}
+
 	/*
-	 * EJECUTAR AUTOMATA	
+	 * EJECUTAR AUTOMATA
 	 */
-	public void ejecutarAutomata(){
-		Scanner imputUsuario = new Scanner(System.in);
+	public void ejecutarAutomata() {
+		String cadenaEntrada;
+		// El usuario inserta la cadena
 		System.out.println("Inserte la cadena a probar:");
-		String cadenaEntrada = imputUsuario.nextLine();
+		Scanner imputUsuario = new Scanner(System.in);
+		cadenaEntrada = imputUsuario.nextLine();
 		imputUsuario.close();
-		//SEPARAR LA CADENA EN LETRAS SEPARADAS
-		for(int i = 0; i < cadenaEntrada.length(); i++){
-			
+		// Empezamos a evaluar la cadena de entrada
+		boolean noTransiciones = true;
+		int numeroIteraciones = cadenaEntrada.length();//hacemos esto porque vamos a ir eliminando la entrada
+		for (int i = 0; i < numeroIteraciones; i++) {
+			if (noTransiciones == true) { //si existen transiciones sigue
+				
+				String simboloTratado = cadenaEntrada.substring(0, 1);
+				cadenaEntrada = cadenaEntrada.substring(1);
+				noTransiciones = false;//suponemos que no hay transiciones
+				
+				for (int j = 0; j < conjuntoTransiciones.size(); j++) {
+					//si encuentra alguna transicion entra
+					if (estadoActual.equals(conjuntoTransiciones.get(j)[0])
+							&& simboloTratado.equals(conjuntoTransiciones.get(j)[1])
+							&& pila.peek().equals(conjuntoTransiciones.get(j)[2])) {
+						estadoActual = conjuntoTransiciones.get(j)[3];
+						pila.pop();
+						//Si la pila tiene mas de un elemento hay que meterlos uno a uno separados
+						if(conjuntoTransiciones.get(j)[4].length() > 1){
+							String aux = conjuntoTransiciones.get(j)[4];
+							for(int k = 0; k < conjuntoTransiciones.get(j)[4].length(); k++){
+								String aux1 = aux.substring(0,1);
+								aux = aux.substring(1);
+								pila.push(aux1);
+							}
+						}else{//si no, no hace falta separar
+							pila.push(conjuntoTransiciones.get(j)[4]);
+						}
+						noTransiciones = true;
+						break;//si se encuentra la transicion pasa al siguiente simbolo de la cadena
+					}
+				}
+			} else{// si no hay transiciones, paramos
+				break;
+			}
+		}//END FOR (NO QUEDAN TRANSICIONES)
+		if(conjuntoF.get(0).equals("@")){//autmata vaciad de pila
+			if(cadenaEntrada.isEmpty() && pila.pop().equals("@")){
+				System.out.println("Cadena aceada!");
+			}
+		}else{
+			for(int i = 0; i < conjuntoF.size(); i++){
+				if(cadenaEntrada.isEmpty() && estadoActual.equals(conjuntoF.get(i))){
+					System.out.println("Cadena aceptada!");
+					break;
+				}
+			}
 		}
 	}
 }
-
